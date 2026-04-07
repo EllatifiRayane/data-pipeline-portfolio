bb56a6f48f8b9cf6975c0d60c3bd51d37f169ad7
Ellatifi Rayane <rayane.ellatifi@outlook.fr>
2026-04-07 11:40:12 +0200

    add customers dbt models

---
+++ ecommerce_analytics/models/marts/dim_customers.sql
@@ -0,0 +1,33 @@
+{{
+  config(
+    materialized = "table"
+  )
+}}
+
+WITH enriched AS (
+    SELECT
+        customer_id,
+        first_name,
+        last_name,
+        gender,
+        city,
+        country,
+        email,
+        customer_segment,
+        birth_date,
+        DATE_DIFF(CURRENT_DATE(), birth_date, YEAR) AS age,
+        registration_date,
+        DATE_DIFF(CURRENT_DATE(), registration_date, DAY) AS customer_tenure_days
+    FROM {{ ref('stg_customers') }}
+)
+
+SELECT
+    *,
+    CASE
+        WHEN age BETWEEN 18 AND 25 THEN '18-25'
+        WHEN age BETWEEN 26 AND 35 THEN '26-35'
+        WHEN age BETWEEN 36 AND 50 THEN '36-50'
+        WHEN age BETWEEN 51 AND 75 THEN '51-75'
+        ELSE 'other'
+    END AS age_group
+FROM enriched
